8fcb971168e6ddd4e57e9338705a42331e2b44f8
NamratagithubCsiber <98451828+NamratagithubCsiber@users.noreply.github.com>
2022-01-28 04:54:53 -0800

    Add files via upload

---
+++ conditionalstat.js
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>if statement</title>
+</head>
+<body>
+<script type="text/javascript">
+const email= prompt("what is your email address?");
+const password = prompt("what is your password?");
+if(email==='namrata@gmail.com' && password==='1234');
+{
+    alert('welcome!');
+}
+else{
+alert('Access Denied!');
+}
+</script>
+</body>
+</html>
